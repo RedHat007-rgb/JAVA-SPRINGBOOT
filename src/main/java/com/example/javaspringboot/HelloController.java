3c19ebbae85f9984f6768b49aa648c591994d545
RedHat007-rgb <cu.16bcs1288@gmail.com>
2024-12-22 13:08:24 +0530

    Done with setup and started learning about writing API'S

---
+++ src/main/java/com/example/javaspringboot/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.javaspringboot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello Java folks";
+    }
+
+    @RequestMapping(value = "/hello/{id}",method = RequestMethod.GET)
+    public String say(@PathVariable String id) {
+        return "Hello "+ id +" " + "Welcome to Java Spring Boot";
+    }
+
+}
+
+
+
